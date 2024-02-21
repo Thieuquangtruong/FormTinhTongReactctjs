9c12f68689ece218ed58ceae413027758dd71db5
tqtruong <thieuquangtruong3861@gmail.com>
2024-02-21 11:03:22 +0700

    cách khác

--- src/App.js
+++ src/App.js
@@ -5,32 +5,23 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 function App() {
-  const [a, setA] = useState();
-  const [b, setB] = useState();
-  const [kq, setKq] = useState();
+ const [a , setA] = useState()
+ const [b , setB] = useState()
+ const [kq , setKq] = useState()
 
-  function valueA(e) {
-    setA(e.target.value)
+function tinhTong(){
+  if(!a || !b){
+    alert("Vui lòng nhập a và b")
+    return null;
   }
+  let total = parseInt(a) + parseInt(b)
+  setKq(total)
+}
 
-  function valueB(e) {
-    setB(e.target.value)
-  }
-
-
-  function tinhTong() {
-    if (!a || !b) {
-      alert('Vui lòng nhập a và b')
-      return;
-    }
-    const total = parseInt(a) + parseInt(b)
-    setKq(total)
-  }
-
-  function xoa() {
-    setA("")
-    setB("")
-  }
+function xoa(){
+  setA("")
+  setB("")
+}
 
   return (
     <div className="App">
@@ -47,7 +38,7 @@ function App() {
           <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="inputGroup-sizing-sm">A</InputGroup.Text>
             <Form.Control
-              value={a} onChange={valueA}
+              value={a} onChange={(e) => setA(e.target.value)}
               aria-label="Small"
               aria-describedby="inputGroup-sizing-sm"
             />
@@ -56,7 +47,7 @@ function App() {
           <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="inputGroup-sizing-sm">B</InputGroup.Text>
             <Form.Control
-              value={b} onChange={valueB}
+              value={b} onChange={(e) => setB(e.target.value)}
               aria-label="Small"
               aria-describedby="inputGroup-sizing-sm"
             />
