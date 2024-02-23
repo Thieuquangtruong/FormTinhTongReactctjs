41d40a657c15b249366263f6fbcad246ca711c40
tqtruong <thieuquangtruong3861@gmail.com>
2024-02-23 15:57:31 +0700

    add button +-*/

--- src/App.js
+++ src/App.js
@@ -5,32 +5,74 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 function App() {
- const [a , setA] = useState()
- const [b , setB] = useState()
- const [kq , setKq] = useState()
+  const [a, setA] = useState()
+  const [b, setB] = useState()
+  const [kq, setKq] = useState()
 
-function tinhTong(){
-  if(!a || !b){
-    alert("Vui lòng nhập a và b")
-    return null;
+  function tinhTong() {
+    if (!a || !b) {
+      alert("Vui lòng nhập a và b")
+      return null;
+    }
+    let total = parseFloat(a) + parseFloat(b)
+    if(!Number.isInteger(total)){
+      total = total.toFixed(1)
+      setKq(total)
+    } else {
+      setKq(total)
+    }
   }
-  let total = parseInt(a) + parseInt(b)
-  setKq(total)
-}
 
-function tinhTru(){
-  if(!a || !b){
-    alert("Vui lòng nhập a và b")
-    return null;
+  function tinhTru() {
+    if (!a || !b) {
+      alert("Vui lòng nhập a và b")
+      return null;
+    }
+    let total = parseFloat(a) - parseFloat(b)
+    if(!Number.isInteger(total)){
+      total = total.toFixed(1)
+      setKq(total)
+    } else {
+      setKq(total)
+    }
   }
-  let total = parseInt(a) - parseInt(b)
-  setKq(total)
-}
 
-function xoa(){
-  setA("")
-  setB("")
-}
+  function tinhNhan() {
+    if (!a || !b) {
+      alert("Vui lòng nhập a và b")
+      return null;
+    }
+    let total = parseFloat(a) * parseFloat(b)
+    if(!Number.isInteger(total)){
+      total = total.toFixed(1)
+      setKq(total)
+    } else {
+      setKq(total)
+    }
+  }
+
+  function tinhChia() {
+    if (!a || !b) {
+      alert("Vui lòng nhập a và b")
+      return null;
+    }
+    if (parseFloat(b) === 0) {
+      alert("không chia được")
+      return;
+    }
+    let total = parseFloat(a) / parseFloat(b)
+    if(!Number.isInteger(total)){
+      total = total.toFixed(1)
+      setKq(total)
+    } else {
+      setKq(total)
+    }
+  }
+  function xoa() {
+    setA("")
+    setB("")
+    setKq("")
+  }
 
   return (
     <div className="App">
@@ -47,6 +89,7 @@ function xoa(){
           <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="inputGroup-sizing-sm">A</InputGroup.Text>
             <Form.Control
+              type='number'
               value={a} onChange={(e) => setA(e.target.value)}
               aria-label="Small"
               aria-describedby="inputGroup-sizing-sm"
@@ -56,6 +99,7 @@ function xoa(){
           <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="inputGroup-sizing-sm">B</InputGroup.Text>
             <Form.Control
+              type='number'
               value={b} onChange={(e) => setB(e.target.value)}
               aria-label="Small"
               aria-describedby="inputGroup-sizing-sm"
@@ -65,6 +109,7 @@ function xoa(){
           <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="inputGroup-sizing-sm">KQ</InputGroup.Text>
             <Form.Control
+              type='number'
               value={kq} disabled
               aria-label="Small"
               aria-describedby="inputGroup-sizing-sm"
@@ -72,14 +117,19 @@ function xoa(){
           </InputGroup>
 
         </div>
-        <div className="d-grid gap-2">
+        <div className="d-grid gap-1">
           <Button variant="primary" onClick={tinhTong}>+</Button>
           <Button variant="primary" onClick={tinhTru}>-</Button>
+          <Button variant="primary" onClick={tinhNhan}>*</Button>
+          <Button variant="primary" onClick={tinhChia}>/</Button>
         </div>
-          <Button variant="danger" onClick={xoa}>Xóa</Button>
+        <Button variant="danger" onClick={xoa}>Xóa</Button>
       </div>
     </div>
   );
 }
+// //1 0
+// 2 2
+// 1.2 3
 
 export default App;
